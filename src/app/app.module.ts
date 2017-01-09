44365eddde183930ed5e67b3320ec459315c686d
Saideep Chhetri <saideep_chhetri@yahoo.com>
2017-01-09 09:55:24 +0000

    navbar setup + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component'
+import { AboutComponent } from './components/about/about.component'
+import { SearchComponent } from './components/search/search.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([ //here we configure our default route & any wild card routes
+      { path: '', component: SearchComponent },
+      { path: 'about', component: AboutComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full' }
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
